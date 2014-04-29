a99fec9e597ed7f8855095392de5eb6940a10929
glenmoe85 <glenmoe85@gmail.com>
2014-04-29 19:53:02 +0100

    First

---
+++ chp1/first.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD// XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<gtml xmlns="http:/www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+<title>Basic PHP page</title>
+
+</head>
+
+<body>
+<p>This is standard html</p>
+
+<?php
+?>
+</body>
+</html>
