ed893345a2ff33af08ed84a0d921507b11112849
glenmoe85 <glenmoe85@gmail.com>
2014-04-29 19:59:45 +0100

    Index

---
+++ chp3/index.php
@@ -0,0 +1,17 @@
+<?php # Script 3.4 - index.php
+
+$page_title = 'Welcome to this Site!';
+
+include ('includes/header.html');
+
+?>
+
+<h1>Content Header</h1>
+
+<p>This is where the page-spec content goes. This section, and the corresponding header, will change from one page to the next.</p>
+
+<p>Volupat at the varius sed sollicituin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisis.</p>
+
+<?php include ('includes/footer.html');
+
+?>
