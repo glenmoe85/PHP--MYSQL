ca444aad2bb94900a3ce40c4db66ea072dc17172
glenmoe85 <glenmoe85@gmail.com>
2014-04-29 19:53:39 +0100

    Strings

---
+++ chp1/strings.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD// XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http:/www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+<title>using strings</title>
+
+</head>
+
+<body>
+<?php #Script 1.6 - strings.php
+
+//Intro into strings:
+
+$first_name = "Glenda";
+$last_name = "Morris";
+$book = "Fluff's ball";
+
+
+
+echo "<p>The book <em>$book</em> was written by $first_name $last_name</p>";
+
+
+?>
+</body>
+</html>
