ec00187ab7da4c45b05249e743e2722762f1b14d
glenmoe85 <glenmoe85@gmail.com>
2014-04-29 19:50:40 +0100

    Constant

---
+++ chp1/constant.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD// XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http:/www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+<title>using constans</title>
+
+</head>
+
+<body>
+<?php #Script 1.9 - constants.php
+
+//intro to constants:
+
+define ('TODAY', 'march 28th 2014');
+
+echo '<p>Today is ' . TODAY . '.<br/> This server is running version <b>' . PHP_OS . '</b> operating system.</p>';
+
+
+?>
+</body>
+</html>
