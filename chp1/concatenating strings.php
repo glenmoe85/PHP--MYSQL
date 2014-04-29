2c90ac8f4baa7c0e2c0659e3e75e2c32b810bcda
glenmoe85 <glenmoe85@gmail.com>
2014-04-29 19:50:31 +0100

    Concatenating strings

---
+++ chp1/concatenating strings.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD// XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http:/www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+<title>using strings</title>
+
+</head>
+
+<body>
+<?php #Script 1.7 - concatenating strings.php
+
+//Concatenating strings:
+
+$city = 'Seattle';
+$state = 'Washington';
+$address = $city . ',' . $state . ', 98101';
+
+
+
+echo "<p>My address is $address</p>";
+
+
+?>
+</body>
+</html>
